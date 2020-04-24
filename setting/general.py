bee7285ed583d6aff8ba6e849225eafaa94b78d2
Anton Kozyriev <sonara4mus@gmail.com>
2020-04-25 02:05:12 +0300

    Correct kernel path

--- setting/general.py
+++ setting/general.py
@@ -1,4 +1,4 @@
-KERNEL_PATH = 'classification/kernel/mnist.h5'
+KERNEL_PATH = 'infrastructure/kernel/mnist.h5'
 TRAIN_SET_PATH = "data/mnist_train_small.csv"
 TEST_SET_PATH = "data/mnist_test.csv"
 IMAGE_RESHAPING_FACTOR = 28
